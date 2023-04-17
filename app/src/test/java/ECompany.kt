02914f0f4ae996dc0f176cc5cee2b7d3d78260b8
A.K <ali.box25020@gmail.com>
2023-04-17 15:00:54 +0330

    cm

--- app/src/test/java/ECompany.kt
+++ app/src/test/java/ECompany.kt
@@ -1,20 +1,13 @@
 class Employee(val firstName: String, val lastName: String, val age: Int, val salary: Int) {
-    fun isWealthy(): Boolean {
-        return salary > 1000
-    }
-
-    override fun toString(): String {
-        return "$firstName $lastName is $age"
-    }
+    fun Wealth() = salary > 1000
+    override fun toString() = "$firstName $lastName is $age"
 }
-
 fun main(){
     // Example usage
-    val employee1 = Employee("Linus", "Torvalds", 53, 2000)
-    val employee2 = Employee("rey", "shms", 21, 800)
-    val employee3 = Employee("Amir", "masoumi", 20 , 3500)
+    val employee1 = Employee("rey", "shms", 21, 800)
+    val employee2 = Employee("Amir", "masoumi", 20 , 3500)
 
-    println(employee3) // Output: Linus Torvalds is 53
-    println(employee3.isWealthy()) // Output: true
+    println(employee1) // Output: Linus Torvalds is 53
+    println(employee1.Wealth()) // Output: true
 
 }
